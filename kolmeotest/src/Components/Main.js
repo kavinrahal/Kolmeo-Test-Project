0131c655b2abcf25597b22d40a2426158182a9b6
Kavin Abeysinghe <s3756717@student.rmit.edu.au>
2022-06-29 13:39:16 +1000

    Basic filter by source functionality

--- kolmeotest/src/Components/Main.js
+++ kolmeotest/src/Components/Main.js
@@ -4,6 +4,9 @@ import Axios from "axios";
 export default function Main(){
     const [articles, setArticles] = useState([]);
     const [selectedSource, setSelectedSource] = useState("");
+    const listOfSources = [...new Set(articles.map(function (item) {
+        return item.source.name === null ? null : item.source.name; 
+    }))];
 
     useEffect(() => {
         Axios.get("https://newsapi.org/v2/top-headlines?language=en&apiKey=96e7efbae84544aca2e40f5834bf2777")
@@ -13,7 +16,14 @@ export default function Main(){
 
     return(
         <div>
-            {articles.map((item) => 
+            <select className="filterBox" value = {selectedSource} onChange = {(e) => setSelectedSource(e.target.value)}>
+                <option value="">All Sources</option>
+                {listOfSources.map((item) => (
+                    <option key = {item} value = {item}>{item}</option>
+                ))}
+            </select>
+            {selectedSource == ""?
+                articles.map((item) => 
                 (
                     <div>
                         <h1>{item.title}</h1>
@@ -21,7 +31,22 @@ export default function Main(){
                         <p>{item.content}</p>
                         <p>{item.source.name}</p>
                     </div>
-                ))}
+                ))
+                :
+                articles.map((item) => 
+                (
+                    item.source.name == selectedSource ?
+                    <div>
+                        <h1>{item.title}</h1>
+                        <h3>{item.author}</h3>
+                        <p>{item.content}</p>
+                        <p>{item.source.name}</p>
+                    </div>
+                    :
+                    null
+                ))
+            
+            }
         </div>
     );
 }
